1bf00068676d7f9f0334f809599120c81e45a2c3
TaiND <giangtt@TaiNDs-Mac-mini.local>
2015-07-14 09:29:27 +0700

    commit core group

---
+++ TechMasterMainProgram/TechMasterMainProgram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TechMasterMainProgram
+//
+//  Created by TaiND on 7/14/15.
+//  Copyright (c) 2015 GiangNX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
