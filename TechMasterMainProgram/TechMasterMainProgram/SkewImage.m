3084ef34495118a7cc3921ac11e93189c97be195
TaiND <giangtt@TaiNDs-Mac-mini.local>
2015-07-22 18:29:43 +0700

    skew image final

--- TechMasterMainProgram/TechMasterMainProgram/SkewImage.m
+++ TechMasterMainProgram/TechMasterMainProgram/SkewImage.m
@@ -26,55 +26,61 @@
     i = 0;
     _isAuto = false;
 //    [_imageSkew setImage:[UIImage imageNamed:@"heart.jpg"]];
-    [_imageSkew setImage:[UIImage imageNamed:@"ngoctrinh.jpg"]];
+    [_imageSkew setImage:[UIImage imageNamed:@"footBall.jpeg"]];
 }
 
 - (IBAction)onChangeValueSlider:(id)sender {
-//    float degree = _slider.value*6.0*pow(-1, i);
-    
-    CATransform3D t = CATransform3DIdentity;
-    t.m34 = -0.005;
-    NSLog(@"pi = %f",M_PI/(180/_slider.value));
-    t = CATransform3DRotate(t, M_PI/(180/_slider.value*2), 0.0, 1.0, 0.0);
-    self.imageSkew.layer.transform = t;
+    if (!_isAuto) {
+        CATransform3D t = CATransform3DIdentity;
+        t.m34 = -0.005;
+        NSLog(@"pi = %f",M_PI/(180/_slider.value));
+        t = CATransform3DRotate(t, M_PI*_slider.value/360, 0.0, 1.0, 0.0);
+        self.imageSkew.layer.transform = t;
+    }
 }
 
 - (IBAction)onCLickAutoButton:(id)sender {
     if (_isAuto) {
         _isAuto = false;
+        i = -1;
+        _slider.hidden = false;
         _button.backgroundColor = [UIColor lightGrayColor];
     }
     else{
         _isAuto = true;
         _button.backgroundColor = [UIColor greenColor];
+        i = 0;
+        _slider.hidden = true;
         [self rotateBallAfterSecond:1 withLoop:10000];
     }
 }
 
 - (void)rotateBallAfterSecond:(int)time withLoop:(int) circle{
-    [UIView animateWithDuration:time animations:^{
-        
-        //        CATransform3D rotatedTransform = _imageSkew.layer.transform;
-        //        rotatedTransform = CATransform3DRotate(rotatedTransform, 30 * M_PI / 180.0, 0.0f, 1.0f, 0.0f);//xoay ngang//(rotatedTransform, 30 * M_PI / 180.0, 0.0f, 0.0f, 1.0f) - xoay doc
-        //        _imageSkew.layer.transform = rotatedTransform;
-        //
-        //        CATransform3D rotatedTransform2 = _imageSkew.layer.transform;
-        //        rotatedTransform2 = CATransform3DRotate(rotatedTransform2, -30 * M_PI / 180.0, 0.0f, 1.0f, 0.0f);
-        //        _imageSkew.layer.transform = rotatedTransform2;
-        
-        //ball
-        
-        i++;
-        float degree = 6.0*pow(-1, i);
-        
-        CATransform3D t = CATransform3DIdentity;
-        t.m34 = -0.005;
-        t = CATransform3DRotate(t, M_PI / degree, 0.0, 1.0, 0.0);
-        self.imageSkew.layer.transform = t;
-        
-    } completion:^(BOOL finished) {
-        [self rotateBallAfterSecond:time withLoop:circle-1];
-    }];
+    if (i>=0) {
+        [UIView animateWithDuration:time animations:^{
+            
+            //        CATransform3D rotatedTransform = _imageSkew.layer.transform;
+            //        rotatedTransform = CATransform3DRotate(rotatedTransform, 30 * M_PI / 180.0, 0.0f, 1.0f, 0.0f);//xoay ngang//(rotatedTransform, 30 * M_PI / 180.0, 0.0f, 0.0f, 1.0f) - xoay doc
+            //        _imageSkew.layer.transform = rotatedTransform;
+            //
+            //        CATransform3D rotatedTransform2 = _imageSkew.layer.transform;
+            //        rotatedTransform2 = CATransform3DRotate(rotatedTransform2, -30 * M_PI / 180.0, 0.0f, 1.0f, 0.0f);
+            //        _imageSkew.layer.transform = rotatedTransform2;
+            
+            //ball
+            
+            i++;
+            float degree = 6.0*pow(-1, i);
+            
+            CATransform3D t = CATransform3DIdentity;
+            t.m34 = -0.005;
+            t = CATransform3DRotate(t, M_PI / degree, 0.0, 1.0, 0.0);
+            self.imageSkew.layer.transform = t;
+            
+        } completion:^(BOOL finished) {
+            [self rotateBallAfterSecond:time withLoop:circle-1];
+        }];
+    }
 
 }
 
